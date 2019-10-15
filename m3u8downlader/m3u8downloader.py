16f7aa084d2206971e40358fe24abe3187205609
ubuntu <ken@localhost.localdomain>
2019-10-15 14:23:00 +0800

    m3u8 downloader

--- m3u8downlader/m3u8downloader.py
+++ m3u8downlader/m3u8downloader.py
@@ -17,14 +17,58 @@ def usage() :
     print("|| -h: help            打印帮助列表        ||")
     print("============================================")
 
+def getBeginUrl(url) :
+    index = url.rfind("/")
+    if index != -1 : 
+        return url[:index+1]
+    return ""
+
 def downSrc(url,path) :
     all_content = requests.get(url,verify = False).text
     file_line = all_content.split("\n")
     if (len(file_line) == 0) | (file_line[0].find("#EXTM3U") == -1) :
         print("\t[error] can't get m3u8 file from url [" + url + "]")
         return False
+    begin_url = getBeginUrl(url) 
+    total = len(file_line)
+    print(file_line)
+    for index, line in enumerate(file_line) :
+        try :
+            if "#EXTINF" in line:
+                tmp = str(file_line[index + 1]).split('/')[-1]
+                sl = len(tmp)
+                tmp = tmp[:sl-1]
+                if(tmp.find("http") != -1) :
+                    pd_url = tmp
+                else :
+                    pd_url = begin_url + tmp;
+                res = requests.get(pd_url, verify=False)
+                c_fule_name = str(file_line[index + 1]).split('/')[-1]
+                sl = len(c_fule_name)
+                c_fule_name = c_fule_name[:sl-1]
+                print c_fule_name
+                with open(path + '/' + c_fule_name, 'ab') as f:
+                    f.write(res.content)
+                    f.flush()
+        except Exception as e:
+            print(e)
+            return False
+    
     return True
 
+def merge(path,out_name) :
+    ts_path = path + "/"
+    path_list = os.listdir(ts_path)
+    path_list.sort()
+    input_name = ""
+    for files in path_list :
+        if files.find('.ts') != -1 :
+            input_name += ts_path+files + '|'
+    sl = len(input_name)
+    input_name = input_name[:sl-1]
+    command = 'ffmpeg -i "concat:%s" -acodec copy -vcodec copy -absf aac_adtstoasc %s'%    (input_name,out_name)
+    os.system(command)
+ 
 
 def exitAndClean(clean_src,path,err) :
     if err :
@@ -32,6 +76,7 @@ def exitAndClean(clean_src,path,err) :
     if clean_src :
         if len(path) != 0 :
             os.rmdir(path)
+    print("\t[info] down")
     sys.exit()
 
 def main(argv,url) :
@@ -73,20 +118,16 @@ def main(argv,url) :
         sys.exit()
     #下载m3u8文件
 
-    if ~downSrc(url,tmppath) :
+    if not downSrc(url,tmppath) :
         exitAndClean(clean_src,tmppath,True)
-    #解析文件 1.内容校验 2.url判断
-
-    #下载
 
     #合并
-    
+    merge(tmppath, out_name) 
 
     #删除零时文件
-    if clean_src :
-        if(len(tmppath) != 0) :
-            os.rmdir(tmppath)
-            print("\t[info] clean m3u8 src files")
+    exitAndClean(clean_src,tmppath,False)
+     
+    
 if __name__ == "__main__":
     if(len(sys.argv) < 2) :
         usage()
